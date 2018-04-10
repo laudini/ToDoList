f22d93b8ea5218389dd60d1493078dbb3dd13b5b
Kamil <krzeminski.yt.kamil@gmail.com>
2018-04-10 13:37:39 +0200

    proper removing of LI

--- js/app.js
+++ js/app.js
@@ -378,15 +378,6 @@ document.addEventListener("DOMContentLoaded", function () {
         myNodelist[q].appendChild(span);
     }
 
-// Click on a close button to hide the current list item
-    var close = document.getElementsByClassName("close");
-    var i;
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
 
 // Add a "checked" symbol when clicking on a list item
     var list = document.querySelector('ul');
@@ -444,7 +435,8 @@ document.addEventListener("DOMContentLoaded", function () {
         var txt = document.createTextNode("\u00D7");
         span.onclick = function () {
             var div = this.parentElement;
-            div.style.display = "none";
+            div.parentElement.removeChild(div);
+            // div.style.display = "none";
 
         };
 
